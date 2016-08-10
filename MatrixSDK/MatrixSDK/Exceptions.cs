370887b3657992db4ee08f3efc5a4603f37590b1
Will Hunt <half-shot@molrams.com>
2016-08-10 16:58:05 +0100

    Exceptions now contain the messages they should have had.

--- MatrixSDK/MatrixSDK/Exceptions.cs
+++ MatrixSDK/MatrixSDK/Exceptions.cs
@@ -23,12 +23,14 @@ namespace MatrixSDK.Exceptions
 	public class MatrixServerError : MatrixException{
 		public readonly MatrixErrorCode ErrorCode;
 		public readonly string ErrorCodeStr;
+		public readonly string Message;
 
 		public MatrixServerError (string errorcode, string message) : base(message){
 			if (!Enum.TryParse<MatrixErrorCode> (errorcode, out ErrorCode)) {
 				ErrorCode = MatrixErrorCode.CL_UNKNOWN_ERROR_CODE;
 			}
 			ErrorCodeStr = errorcode;
+			Message = message;
 		}
 
 	}

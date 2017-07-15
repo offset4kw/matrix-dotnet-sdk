31cc1f6f81b81739ef5aa21334b68d3eaf5cd503
Will Hunt <half-shot@molrams.com>
2017-07-15 13:50:21 +0100

    Create MatrixSpecVersionAttribute

--- Matrix/MatrixAttributes.cs
+++ Matrix/MatrixAttributes.cs
@@ -1,19 +1,36 @@
-﻿using System;
+using System;
 
 namespace Matrix
 {
-	[AttributeUsage(AttributeTargets.Method)]
-	public class MatrixSpec : Attribute
-	{
-		const string MATRIX_SPEC_URL = "http://matrix.org/docs/spec/";
-		public readonly string URL;
-		public MatrixSpec(string url){
-			URL = MATRIX_SPEC_URL + url;
-		}
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MatrixSpec : Attribute
+    {
+        const string MATRIX_SPEC_URL = "http://matrix.org/docs/spec/";
+        public readonly string URL;
+        public MatrixSpec(string url){
+            URL = MATRIX_SPEC_URL + url;
+        }
 
-		public override string ToString ()
-		{
-			return string.Format (URL);
-		}
-	}
+        public override string ToString ()
+        {
+            return string.Format (URL);
+        }
+    }
+
+    /// <summary>
+    /// The versions this method
+    /// </summary>
+    [AttributeUsage(
+    AttributeTargets.Method |
+    AttributeTargets.Property |
+    AttributeTargets.Class |
+    AttributeTargets.Struct |
+    AttributeTargets.Field
+    )]
+    public class MatrixSpecVersionAttribute : Attribute {
+        public readonly string[] Versions;
+        public MatrixSpecVersionAttribute(params string[] versions) {
+            Versions = versions;
+        }
+    }
 }

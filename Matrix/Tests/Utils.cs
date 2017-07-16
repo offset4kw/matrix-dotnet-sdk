250674919ce5636ef422d55e6488a54ed3f9985a
Will Hunt <half-shot@molrams.com>
2017-07-16 10:20:46 +0100

    Add state_key and age to the MockEvent function

--- Matrix/Tests/Utils.cs
+++ Matrix/Tests/Utils.cs
@@ -5,10 +5,17 @@ namespace Matrix.Tests
 {
     public class Utils
     {
-        public static MatrixEvent MockEvent(MatrixEventContent content)
+        public static MatrixEvent MockEvent(
+            MatrixEventContent content,
+            string state_key = null,
+            int age = 0)
         {
             MatrixEvent ev = new MatrixEvent();
             ev.content = content;
+            if(state_key != null) {
+                ev.state_key = state_key;
+            }
+            ev.age = age;
             return ev;
         }
     }

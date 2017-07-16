3dc1e18737e55c552f23f415a2ca9b8b07d57f60
Will Hunt <half-shot@molrams.com>
2017-07-16 12:30:40 +0100

    Create a testing backend

---
+++ Matrix/Tests/Backend/TestingAPIBackend.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+namespace Matrix.Backends
+{
+	public class TestingAPIBackend : IMatrixAPIBackend
+	{
+		public MatrixRequestError Get  (string apiPath, bool authenticate, out JObject result) {
+            result = null;
+            return null;
+        }
+
+		public MatrixRequestError Post (string apiPath, bool authenticate, JObject request, out JObject result) {
+            result = null;
+            return null;
+        }
+
+		public MatrixRequestError Post (string apiPath, bool authenticate, JObject request, Dictionary<string,string> headers, out JObject result) {
+            result = null;
+            return null;
+        }
+
+		public MatrixRequestError Post (string apiPath, bool authenticate, byte[] request , Dictionary<string,string> headers, out JObject result) {
+            result = null;
+            return null;
+        }
+        
+		public MatrixRequestError Put  (string apiPath, bool authenticate, JObject request, out JObject result) {
+            result = null;
+            return null;
+        }
+
+		public void SetAccessToken(string access_token) {
+
+        }
+	}
+}
+

7f0aa9fec1e41886eccc295c26f2fa60527b9391
Will Hunt <will@half-shot.uk>
2019-02-16 02:15:51 +0000

    Can specify a last spec number
    Include utilities to parse to and from spec versions

--- Matrix/MatrixAttributes.cs
+++ Matrix/MatrixAttributes.cs
@@ -5,20 +5,22 @@ namespace Matrix
     [AttributeUsage(AttributeTargets.Method)]
     public class MatrixSpec : Attribute
     {
-        public EMatrixSpecApiVersion Version { get; }
+        public EMatrixSpecApiVersion MinVersion { get; }
+        public EMatrixSpecApiVersion LastVersion { get; }
         public EMatrixSpecApi Api { get; }
         public string Path { get; }
         private const string MATRIX_SPEC_URL = "http://matrix.org/docs/spec/";
-        public MatrixSpec(EMatrixSpecApiVersion supportedVer, EMatrixSpecApi api, string path)
+        public MatrixSpec(EMatrixSpecApiVersion supportedVer, EMatrixSpecApi api, string path, EMatrixSpecApiVersion lastVersion = EMatrixSpecApiVersion.Unknown)
         {
             Api = api;
             Path = path;
-            Version = supportedVer;
+            MinVersion = supportedVer;
+            LastVersion = lastVersion;
         }
 
         public override string ToString ()
         {
-            var verStr = "";
+            var verStr = GetStringForVersion(LastVersion);
             var apiStr = "";
             switch (Api)
             {
@@ -31,47 +33,63 @@ namespace Matrix
                 default:
                     throw new ArgumentOutOfRangeException(nameof(Api), Api, null);
             }
-            
-            switch (Version)
+            return $"{MATRIX_SPEC_URL}/{apiStr}/{verStr}.html#${Path}";
+        }
+
+        public static string GetStringForVersion(EMatrixSpecApiVersion version)
+        {
+            switch (version)
             {
                 case EMatrixSpecApiVersion.R001:
-                    verStr = "r0.0.1";
-                    break;
+                    return "r0.0.1";
                 case EMatrixSpecApiVersion.R010:
-                    verStr = "r0.1.0";
-                    break;
+                    return "r0.1.0";
                 case EMatrixSpecApiVersion.R020:
-                    verStr = "r0.2.0";
-                    break;
+                    return "r0.2.0";
                 case EMatrixSpecApiVersion.R030:
-                    verStr = "r0.3.0";
-                    break;
+                    return "r0.3.0";
                 case EMatrixSpecApiVersion.R040:
-                    verStr = "r0.4.0";
-                    break;
-                // case EMatrixSpecApiVersion.Unstable:
+                    return "r0.4.0";
                 default:
-                    verStr = "unstable";
-                    break;
+                    return "unstable";
             }
-            return $"{MATRIX_SPEC_URL}/{apiStr}/{verStr}.html#${Path}";
+        }
+        
+        public static EMatrixSpecApiVersion GetVersionForString(string version)
+        {
+            switch (version)
+            {
+                case "r0.0.1":
+                    return EMatrixSpecApiVersion.R001;
+                case "r0.1.0":
+                    return EMatrixSpecApiVersion.R010;
+                case "r0.2.0":
+                    return EMatrixSpecApiVersion.R020;
+                case "r0.3.0":
+                    return EMatrixSpecApiVersion.R030;
+                case "r0.4.0":
+                    return EMatrixSpecApiVersion.R040;
+                default:
+                    return EMatrixSpecApiVersion.Unknown;
+            }
         }
     }
 
     public enum EMatrixSpecApi
     {
         ClientServer,
-        ApplicationService,
+        ApplicationService
     }
     
     public enum EMatrixSpecApiVersion
     {
+        Unknown,
         Unstable,
         R001,
         R010,
         R020,
         R030,
-        R040,
+        R040
     }
 
     /// <summary>
